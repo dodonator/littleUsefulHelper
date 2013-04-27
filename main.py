d3080ca929bc0a30bf98e9358bc672e1e0371102
Dodo <dodo@dodo@chaosdorf.de>
2013-04-27 20:26:13 +0200

    "foo"

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import random
+import math
+import time
+import datetime
+class timeTools(object):
+	def getTime(self):
+		localTime = {}
+		lTime = time.localtime()
+		localTime['year'] = lTime[0]
+		localTime['month'] = lTime[1] 
+		localTime['day'] = lTime[2]
+		localTime['hour'] = lTime[3]
+		localTime['minute'] = lTime[4]
+		localTime['second'] = lTime[5]
+		return localTime
+	def formatTimeDict(self,timeDict):
+		result = ''
+		result = str(timeDict['hour']) + ':' + str(timeDict['minute']) + ':' + str(timeDict['second'])
+		result += ' ' + str(timeDict['day']) + '.' + str(timeDict['month']) + '.' + str(timeDict['year'])
+		return result
+	def slS(self,sec): # sleep seconds
+		time.sleep(sec)
+	def slM(self,mon): # sleep minutes
+		time.sleep(mon*60)
+	def slH(self,hour): # sleep hours
+		time.sleep(hour*3600)	
+class generalTools(object):
+	def repString(self,string,rep):
+		result = rep*string
+		return result	
+	def wc(self,sec): # wait and clear
+		time.sleep(sec)
+		os.system('clear')
+	def cw(self,sec): # clear and wait
+		os.system('clear')
+		time.sleep(sec)
+	def CL(self):
+		os.system('clear')
+timeT = timeTools()
+gT = generalTools()
+
